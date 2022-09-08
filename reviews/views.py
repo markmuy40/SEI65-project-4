865e2c1a5a64d98e20378ed65117cf0d21054a4f
Mark Muyuela <mark.muyuela81@gmail.com>
2022-09-08 18:53:28 +0100

    review crud functionality tested and working

--- reviews/views.py
+++ reviews/views.py
@@ -9,7 +9,7 @@ from .serializers.common import ReviewSerializer
 # Create your views here.
 class ReviewListView(APIView):
 
-    # ! get all reviews
+    # get all reviews
     def get(self, _request):
         reviews = Review.objects.all()
         print("reviews->", reviews)
@@ -17,7 +17,7 @@ class ReviewListView(APIView):
         print(serialized_reviews)
         return Response(serialized_reviews.data, status=status.HTTP_200_OK)
 
-    # ! post a review
+    # post a review
     def post(self, request):
         print('request data->', request.data)
         review_to_add = ReviewSerializer(data=request.data)
@@ -29,28 +29,30 @@ class ReviewListView(APIView):
             print('ERROR')
             return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+
 class ReviewDetailView(APIView):
 
-    # ! function to fetch data
+    # function to fetch data
     def get_review(self, pk):
         try:
-            return Review.objects.get(pk)
+            return Review.objects.get(pk=pk)
         except Review.DoesNotExist:
-            raise NotFound(detail='Review not found 😢')
+            raise NotFound(detail="Review not found")
 
-    # ! function to get a single review
+    # function to get a single review
     def get(self, _request, pk):
         review = self.get_review(pk=pk)
+        print("single review return endpoint")
         serialized_review = ReviewSerializer(review)
         return Response(serialized_review.data)
 
-    # ! function to delete a review
+    # function to delete a review
     def delete(self, _request, pk):
         review_to_delete = self.get_review(pk=pk)
         review_to_delete.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)  
 
-    # ! function to update a review
+    # function to update a review
     def put(self, request, pk):
         review_to_update = self.get_review(pk=pk)
         updated_review = ReviewSerializer(review_to_update, data=request.data)
